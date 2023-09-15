555a5b5d50e24a1c951d884e4038cf0044fdf6ed
obadia <obadia.maasai@student.moringaschool.com>
2023-09-15 22:07:49 +0300

    complete

--- lib/list_crud.py
+++ lib/list_crud.py
@@ -1,26 +1,67 @@
-def create_an_empty_list():
-    return None
+# def create_an_empty_list():
+#     return []
+# create_an_empty_list()
+
 
 def create_a_list():
+    new_list = [1, 2, 3, 4]
+    my_list = create_a_list()
+    print("Initial List:", my_list)
+    return new_list
+
+def add_element_to_end_of_list(my_list, element):
+    my_list.append(element)
+    add_element_to_end_of_list(my_list, 5)
+    print("After Adding to End:", my_list)
+
+def add_element_to_start_of_list(my_list, element):
+    my_list.insert(0, element)
+    add_element_to_start_of_list(my_list, 0)
+    print("After Adding to Start:", my_list)
+
+def remove_element_from_end_of_list(my_list):
+    if len(my_list) > 0:
+        my_list.pop()
+        add_element_to_start_of_list(my_list, 0)
+        print("After Adding to Start:", my_list)
+
+def remove_element_from_start_of_list(my_list):
+    if len(my_list) > 0:
+        del my_list[0]
+        remove_element_from_end_of_list(my_list)
+        print("After Removing from End:", my_list)
+
+
+def retrieve_first_element_from_list(my_list):
+    if len(my_list) > 0:
+        return my_list[0]
     return None
 
-def add_element_to_end_of_list(l, element):
+def retrieve_element_from_index(my_list, index):
+    if 0 <= index < len(my_list):
+        return my_list[index]
     return None
 
-def add_element_to_start_of_list(l, element):
+def retrieve_last_element_from_list(my_list):
+    if len(my_list) > 0:
+        return my_list[-1]
     return None
 
-def remove_element_from_end_of_list(l):
-    return None
 
-def remove_element_from_start_of_list(l):
-    return None
+ 
 
-def retrieve_first_element_from_list(l):
-    return None
 
-def retrieve_element_from_index(l, index):
-    return None
 
-def retrieve_last_element_from_list(l):
-    return None
+
+
+remove_element_from_start_of_list(my_list)
+print("After Removing from Start:", my_list)
+
+first_element = retrieve_first_element_from_list(my_list)
+print("First Element:", first_element)
+
+element_at_index_2 = retrieve_element_from_index(my_list, 2)
+print("Element at Index 2:", element_at_index_2)
+
+last_element = retrieve_last_element_from_list(my_list)
+print("Last Element:", last_element)
